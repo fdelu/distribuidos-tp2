109e25df3029dc7768522b49ae6af2040a52f228
fdelu <fdeluca@fi.uba.ar>
2023-05-31 21:21:02 -0300

    Handle generics correctly in serde

--- src/system/common/serde/internal/deserialize.py
+++ src/system/common/serde/internal/deserialize.py
@@ -1,39 +1,49 @@
 from enum import EnumType
 import json
 import types
-from typing import Any, Type, TypeVar, get_args, get_origin, get_type_hints, cast
+from typing import Any, Type, TypeVar, get_args, get_origin, get_type_hints, cast, Union
 
-from .util import (
-    SIMPLE_TYPES,
-    SerdeError,
-    Translated,
-    verify_type,
-)
+from .util import SIMPLE_TYPES, SerdeError, Translated
 
 T = TypeVar("T")
+TypeVars = dict[str, Type[Any]]
 
 
-def deserialize_item(data_type: Any, data: Translated) -> Any:
+def deserialize_item(data_type: Any, data: Translated, type_vars: TypeVars) -> Any:
     if data_type in SIMPLE_TYPES:
-        verify_type(data, data_type)
-        return data
-    if isinstance(data_type, types.GenericAlias):
-        return deserialize_generic(data_type, data)
-    if isinstance(data_type, types.UnionType):
-        return deserialize_union(data_type, data)
+        return deserialize_simple(data_type, data)
+
+    origin = get_origin(data_type)
+    if isinstance(data_type, types.UnionType) or origin is Union:
+        return deserialize_union(data_type, data, type_vars)
+
+    if origin:
+        return deserialize_generic(origin, get_args(data_type), data, type_vars)
     if isinstance(data_type, EnumType):
         return deserialize_enum(data_type, data)
-    return deserialize_object(data_type, data)
+    if isinstance(data_type, TypeVar):
+        return deserialize_item(type_vars[data_type.__name__], data, type_vars)
+    return deserialize_object(data_type, data, type_vars)
+
+
+def deserialize_simple(data_type: type, data: Translated) -> Any:
+    if (
+        data_type == float
+        and isinstance(data_type, (int, float))
+        or type(data) == data_type
+    ):
+        return data
+    raise SerdeError(f"Expected {data_type}, got {type(data)}")
 
 
 def deserialize_enum(type_info: EnumType, data: Translated) -> Any:
     return type_info(data)
 
 
-def deserialize_union(type_info: types.UnionType, data: Translated) -> Any:
+def deserialize_union(type_info: Any, data: Translated, tv: TypeVars) -> Any:
     for t in get_args(type_info):
         try:
-            return deserialize_item(t, data)
+            return deserialize_item(t, data, tv)
         except SerdeError:
             pass
     raise SerdeError(
@@ -41,33 +51,60 @@ def deserialize_union(type_info: types.UnionType, data: Translated) -> Any:
     )
 
 
-def deserialize_generic(type_info: types.GenericAlias, data: Translated) -> Any:
-    expected_type: Any = get_origin(type_info)
+def add_type_vars(base: type, args: tuple[Any, ...], tv: TypeVars) -> TypeVars:
+    type_vars = tv.copy()
+    if not hasattr(base, "__orig_bases__"):
+        return type_vars
+
+    for parent in getattr(base, "__orig_bases__"):
+        for n, arg in enumerate(get_args(parent)):
+            if not isinstance(args[n], TypeVar):
+                type_vars[arg.__name__] = args[n]
+    return type_vars
+
+
+def deserialize_generic(
+    origin: type, args: tuple[Any, ...], data: Translated, tv: TypeVars
+) -> Any:
+    updated_tv = add_type_vars(origin, args, tv)
+    if origin in (list, set, dict, tuple):
+        item = deserialize_collection(origin, args, data, updated_tv)
+    else:
+        item = deserialize_item(origin, data, updated_tv)
+    return item
+
+
+def deserialize_collection(
+    origin: type, args: tuple[Any, ...], data: Translated, tv: TypeVars
+) -> Any:
     if not isinstance(data, list):
-        raise SerdeError(f"Generic type {expected_type} must be serialized as a list")
+        raise SerdeError(f"Can't deserialize {origin}: serialized object is not a list")
+
     data = cast(list[Translated], data)
-    if expected_type in (list, set):
-        item_type = get_args(type_info)[0]
-        values = expected_type(deserialize_item(item_type, i) for i in data)
+    if origin in (list, set):
+        item_type = args[0]
+        values = origin(deserialize_item(item_type, i, tv) for i in data)
         return values
-    if expected_type == dict:
-        key_type, value_type = get_args(type_info)
+    if origin == dict:
+        key_type, value_type = args
         values = {}
         for pair in data:
             if not isinstance(pair, list) or len(pair) != 2:
                 raise SerdeError("Dicts must be serialized as lists of pairs")
             pair = cast(list[Translated], pair)
             k, v = pair
-            values[deserialize_item(key_type, k)] = deserialize_item(value_type, v)
+            values[deserialize_item(key_type, k, tv)] = deserialize_item(
+                value_type, v, tv
+            )
         return values
-    if expected_type == tuple:
-        item_types = get_args(type_info)
-        values = tuple(deserialize_item(t, d) for t, d in zip(item_types, data))
+    if origin == tuple:
+        item_types = args
+        values = tuple(deserialize_item(t, d, tv) for t, d in zip(item_types, data))
         return values
-    raise SerdeError(f"Generic type {expected_type} is not supported")
+    raise SerdeError(f"Unknown collection type {origin}")
 
 
-def deserialize_object(data_type: Type[T], data: Translated) -> T:
+def deserialize_object(data_type: Type[T], data: Translated, type_vars: TypeVars) -> T:
     type_hints = get_type_hints(data_type)
     out = data_type.__new__(data_type)
     if not isinstance(data, list):
@@ -83,11 +120,11 @@ def deserialize_object(data_type: Type[T], data: Translated) -> T:
             f"Object name {data_type.__name__} does not match serialized name {data[0]}"
         )
     for i, (name, item_type) in enumerate(type_hints.items(), start=1):
-        item = deserialize_item(item_type, data[i])
-        verify_type(item, item_type)
+        item = deserialize_item(item_type, data[i], type_vars)
         object.__setattr__(out, name, item)
     return out
 
 
 def deserialize(data_type: Any, data: str) -> Any:
-    return deserialize_item(data_type, json.loads(data))
+    type_vars: TypeVars = {}
+    return deserialize_item(data_type, json.loads(data), type_vars)
--- src/system/common/serde/internal/util.py
+++ src/system/common/serde/internal/util.py
@@ -1,5 +1,4 @@
 import types
-from typing import Callable, Concatenate, ParamSpec, get_args, get_origin
 
 """
 Type alias for the types that can be serialized and deserialized.
@@ -7,41 +6,6 @@ Type alias for the types that can be serialized and deserialized.
 Translated = list["Translated"] | str | int | float | bool | None
 
 SIMPLE_TYPES = [str, int, float, types.NoneType, bool]
-
-P = ParamSpec("P")
-Serializer = Callable[Concatenate[Translated, P], str]
-Deserializer = Callable[Concatenate[str, P], Translated]
-
-
-def verify_type(item: object, expected_type: type) -> None:
-    """
-    Verifies that the given item is of the expected type.
-
-    Does not verify generic types, only the base type.
-    """
-
-    if isinstance(expected_type, types.UnionType):
-        for t in get_args(expected_type):
-            try:
-                verify_type(item, t)
-                return
-            except SerdeError:
-                pass
-
-    if isinstance(expected_type, types.GenericAlias):
-        base_type = get_origin(expected_type)
-        if base_type == type(item):
-            return
-
-    if expected_type == float and isinstance(item, (int, float)):
-        return
-
-    if type(item) == expected_type:
-        return
-
-    raise SerdeError(
-        f"Expected type {expected_type} but got {type(item)} (value: {item}))"
-    )
 
 
 class SerdeError(Exception):

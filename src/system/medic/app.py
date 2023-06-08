266c2a64744b62db2a190379803b9396a5f61b25
Tomas Civini <tcivini@gmail.com>
2023-06-07 23:40:26 -0300

    minor changes, abandoning this idea

--- src/system/medic/app.py
+++ src/system/medic/app.py
@@ -4,8 +4,8 @@ from shared.log import setup_logs
 from .config import Config
 import yaml
 from docker.types import Mount
-import os
 from typing import Any, Optional, cast
+import subprocess
 
 
 def parse_compose() -> dict[str, Any]:
@@ -32,24 +32,41 @@ def make_image_name(proyect_name: str, service_name: str) -> str:
     return f"{proyect_name}-{service_name}"
 
 
+def get_replication_factor(service_compose: Any, config: Config) -> Optional[int]:
+    try:
+        replica_env = cast(str, service_compose["deploy"]["replicas"])
+        replica_strip = replica_env.split("{")[1].split("}")[0]
+        replica_int = int(config.get_env(replica_strip))
+        return replica_int
+    except Exception as e:
+        logging.error(f"error getting replica factor, {replica_strip}: {e}")
+        return 1
+
+
 def main() -> None:
     config = Config()
     setup_logs(config.log_level)
-    abs_path = cast(str, os.environ.get("ABS_PATH"))
-    logging.info(f"started medic{abs_path}")
-    compose = parse_compose()
-    if not compose:
-        logging.error("compose.yaml couldnt be parsed")
-        return
+    logging.info("started medic")
+    # compose = parse_compose()
+    # if not compose:
+    #     logging.error("compose.yaml couldnt be parsed")
+    #     return
 
-    docker_client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+    # docker_client = docker.DockerClient(base_url='unix://var/run/docker.sock')
 
-    mount = create_mount(compose["services"]["client"], abs_path)
-    container = docker_client.containers.run(
-        'distribuidos-tp2-client', detach=True, name='distribuidos-tp2-client',
-        mounts=mount
-    )
-    print(container.logs().decode('utf-8'))
+    # mount = create_mount(compose["services"]["year-joiners"], config.abs_path)
+    # name = make_image_name(compose["name"], "year-joiners")
+    # replication = get_replication_factor(compose["services"]["year-joiners"], config)
+    # logging.info(f"about to run container: {replication}")
+    # container = docker_client.containers.run(
+    #     name, detach=True, name=name,
+    #     mounts=mount
+    # )
+    # print(container.logs().decode('utf-8'))
+    # logging.info("finished running container")
+    result = subprocess.run(['docker', 'start', 'caca'],
+                            check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    logging.info(f"Command executed. Result={result.returncode}. Output={result.stdout}. Error={result.stderr}")
 
 
 main()
--- src/system/medic/config.py
+++ src/system/medic/config.py
@@ -1,9 +1,16 @@
 from common.config_base import ConfigBase
+import os
+from typing import cast
 
 
 class Config(ConfigBase):
     address: str
     prefetch_count: int
+    abs_path: str
 
     def __init__(self) -> None:
         super().__init__()
+        self.abs_path = cast(str, os.environ.get("ABS_PATH"))
+
+    def get_env(self, key: str) -> str:
+        return cast(str, os.environ.get(key))

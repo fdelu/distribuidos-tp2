64e09b76f8a8a4d802ac44ff596b5a888fe3f3cf
Tomas Civini <tcivini@gmail.com>
2023-06-23 14:59:09 -0300

    minor refactor

--- src/system/medic/bully.py
+++ src/system/medic/bully.py
@@ -1,9 +1,7 @@
 import logging
-import subprocess
 from typing import Any
 from .config import Config
 from common.comms_base.util import set_healthy
-from .compose_parser import get_containers
 from .system_communication import SystemCommunication
 from .messages.bully_messages import (
     BullyMessage,
@@ -14,83 +12,7 @@ from .messages.bully_messages import (
     AliveLeaderMessage,
 )
 from .leader_heartbeat import LeaderChecker, LeaderHeartbeat
-
-
-class HealthMonitor:
-    comms: SystemCommunication
-    is_leader: bool
-    started: bool
-    id: int
-    timer_dict: dict[str, Any]
-    container_list: list[str]
-    config: Config
-
-    def __init__(self, comms: SystemCommunication, id: int, config: Config) -> None:
-        self.comms = comms
-        self.is_leader = True
-        self.started = False
-        self.id = id
-        self.timer_dict = {}
-        self.container_list = get_containers(config, id)
-        self.config = config
-        # logging.info(f"container list: {self.container_list}")
-        # TODO: maybe auto start main medic
-
-    def start_timers(self) -> None:
-        # start a timer for each container
-        for container_name in self.container_list:
-            self.timer_dict[container_name] = self.comms.set_timer(
-                lambda: self.container_dead(container_name),
-                self.config.first_heartbeat_timeout
-            )
-            # time to consider a container dead if no previous heartbeats
-            # have been received
-
-    def im_leader(self) -> None:
-        if not self.is_leader or not self.started:
-            self.is_leader = True
-            self.started = True
-            self.comms.bind_heartbeat_route()
-            self.start_timers()
-
-    def stop_timers(self) -> None:
-        for timer in self.timer_dict.values():
-            self.comms.cancel_timer(timer)
-
-    def im_not_leader(self) -> None:
-        if self.is_leader:
-            self.is_leader = False
-            self.comms.unbind_heartbeat_route()
-            self.stop_timers()
-            self.start_heartbeat()
-
-    def send_heartbeat(self) -> None:
-        if not self.is_leader:
-            self.comms.send(AliveMessage(self.comms.name))
-            self.comms.set_timer(self.send_heartbeat, self.config.heartbeat_interval)
-            # time between heartbeats
-
-    def start_heartbeat(self) -> None:
-        self.send_heartbeat()
-
-    def restart_container_timer(self, container_name: str, time: int) -> None:
-        if container_name in self.timer_dict:
-            self.comms.cancel_timer(self.timer_dict[container_name])
-        self.timer_dict[container_name] = self.comms.set_timer(
-            lambda: self.container_dead(container_name), time
-        )
-
-    def handle_heartbeat(self, message: AliveMessage) -> None:
-        logging.debug(f"Received heartbeat from {message.container_name}")
-        self.restart_container_timer(message.container_name,
-                                     self.config.heartbeat_timeout)
-        # time to consider a container dead
-
-    def container_dead(self, container_name: str) -> None:
-        logging.info(f"Container {container_name} dead")
-        subprocess.Popen(["docker", "start", container_name])
-        self.restart_container_timer(container_name, self.config.restart_timeout)
-        # time to consider a container dead after restarting it
+from .health_monitor import HealthMonitor
 
 
 class Bully:
@@ -191,11 +113,14 @@ class Bully:
         logging.info("Coordinator timeout")
         self.start_election()
 
-    def handle_answer(self, message: AnswerMessage) -> None:
-        self.received_awnser = True
+    def cancel_awnser_timer(self) -> None:
         if self.awnser_timer_id is not None:
             self.comms.cancel_timer(self.awnser_timer_id)
             self.awnser_timer_id = None
+
+    def handle_answer(self, message: AnswerMessage) -> None:
+        self.received_awnser = True
+        self.cancel_awnser_timer()
         self.coordination_timer_id = self.comms.set_timer(
             self.__timer_coordinator, self.config.coordinator_timeout)
         # time that waits for a CoordinatorMessage or restart election
@@ -205,9 +130,7 @@ class Bully:
         if self.coordination_timer_id is not None:
             self.comms.cancel_timer(self.coordination_timer_id)
             self.coordination_timer_id = None
-        if self.awnser_timer_id is not None:
-            self.comms.cancel_timer(self.awnser_timer_id)
-            self.awnser_timer_id = None
+        self.cancel_awnser_timer()
         self.current_leader = message.id_coordinator
         self.election_started = False
         self.is_leader = False
